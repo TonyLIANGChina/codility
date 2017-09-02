2aefeb5bed9571e6baf5a6564ff43cadc72b0b25
Tony_LIANG <tony_liang_job@hotmail.com>
2017-09-02 21:41:17 +0800

    Create ArrayRotated.c

---
+++ ArrayRotated.c
@@ -0,0 +1,18 @@
+struct Results solution(int A[], int N, int K) 
+{
+	Results result;
+	int i;
+	int *temp;
+	temp = (int *)malloc(N * sizeof(int));
+	memcpy(temp, A, N * sizeof(int));
+	for (i = 0; i < N; i++)
+	{
+		temp[(i + K) % N] = A[i];
+	}
+	result.A = temp;
+	result.N = N;
+	return result;
+	free(temp);
+	temp = NULL;
+
+}
